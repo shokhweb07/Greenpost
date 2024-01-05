54fd6da93a01a2945066339735b76d7ffb65f267
bekhdeveloper <developerbekh@gmail.com>
2024-01-05 15:56:12 +0500

    developer

--- js/main.js
+++ js/main.js
@@ -1,3 +1,34 @@
+document.getElementById("menyular").addEventListener("mouseover", mouseOver);
+document.getElementById("menyular").addEventListener("mouseout", mouseOut);
+
+function mouseOver() {
+  document.getElementById("menu-block").style.display = "block";
+}
+
+function mouseOut() {
+  document.getElementById("menu-block").style.display = "none";
+}
+
+
+
+document.getElementById("ftMenyular").addEventListener("mouseover", mouseOver1);
+document.getElementById("ftMenyular").addEventListener("mouseout", mouseOut1);
+
+function mouseOver1() {
+  document.getElementById("ftMenuBlock").style.display = "block";
+}
+
+function mouseOut1() {
+  document.getElementById("ftMenuBlock").style.display = "none";
+}
+
+function menuOpen(){
+  document.getElementById('menu-wrapper').style.display = "block"
+}
+
+function exitMenu(){
+  document.getElementById('menu-wrapper').style.display = "none"
+}
 function registratsiya(){
     document.getElementById('vxod').style.marginBottom = '0px';
     document.getElementById('vxod').style.background = '#f8f9fa'; 
