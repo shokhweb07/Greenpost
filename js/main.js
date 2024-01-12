35a7f92903cf2c9e0dd98815fdb576bc2fed3f8e
bekhdeveloper <developerbekh@gmail.com>
2024-01-12 10:54:05 +0500

    bekhdeveloper

--- js/main.js
+++ js/main.js
@@ -51,6 +51,93 @@ function vxod(){
   document.getElementById('login-wrap1').style.marginTop = '20vh'
 }
 
+// 
+
+function saveModal(){
+  var inp1 = document.getElementById('fio')
+  var inp2 = document.getElementById('tel-nomer')
+  var inp3 = document.getElementById('region')
+  var inp4 = document.getElementById('rayon')
+  var inp5 = document.getElementById('pinfl')
+  var inp6 = document.getElementById('ulitsa')
+  var inp7 = document.getElementById('dom')
+  var inp8 = document.getElementById('kvartira')
+  var trs = document.getElementById('trs')
+  var tr = document.createElement('tr')
+  var th1 = document.createElement('th')
+  var th2 = document.createElement('th')
+  var th3 = document.createElement('th')
+  var th4 = document.createElement('th')
+  var th5 = document.createElement('th')
+  var th6 = document.createElement('th')
+  var th7 = document.createElement('th')
+  var th8 = document.createElement('th')
+  var th9 = document.createElement('th')
+  var hisob = 0
+
+  tr.style.width = '100%'
+  tr.style.display = 'grid'
+  tr.style.gridTemplateColumns = '1.35fr 2.58fr 6.65fr 3.3fr 5.7fr 7.25fr 3fr 5.85fr 5fr'
+
+
+  th9.innerText = hisob + 1
+  th1.innerText = inp1.value
+  th2.innerText = inp2.value
+  th3.innerText = inp3.value
+  th4.innerText = inp4.value
+  th5.innerText = inp5.value
+  th6.innerText = inp6.value
+  th7.innerText = inp7.value + '/' + inp8.value
+  th8.innerText = 'upravlenie'
+  trs.appendChild(tr)
+  
+  tr.appendChild(th9)
+  tr.appendChild(th1)
+  tr.appendChild(th2)
+  tr.appendChild(th3)
+  tr.appendChild(th4)
+  tr.appendChild(th5)
+  tr.appendChild(th6)
+  tr.appendChild(th7)
+  tr.appendChild(th8)
+}
+
+function openMain2(){
+  document.getElementById('main').style.display = 'none'
+  document.getElementById('main2').style.display = 'block'
+}
+
+function exitMain2(){
+  document.getElementById('main2').style.display = 'none'
+  document.getElementById('main').style.display = 'block'
+}
+
+
+function onEmpty(){
+  document.getElementById('asosiy').style.left = '-200px'
+  document.getElementById('empty-qism').style.display = 'none'
+}
+
+function onBurger(){
+  document.getElementById('asosiy').style.left = '0'
+  document.getElementById('empty-qism').style.display = 'block'
+}
+
+function contactOn(){
+  document.getElementById('kontakti').style.display = 'block'
+  document.getElementById('kontakti').style.marginRight = '0'
+  document.getElementById('empty-qism1').style.display = 'block'
+}
+
+function contactOff(){
+  document.getElementById('kontakti').style.display = 'none'
+  document.getElementById('kontakti').style.marginRight = '-270px'
+  document.getElementById('empty-qism1').style.display = 'none'
+}
+
+function voyti(){
+  
+}
 // admin panel
 function hideMenu1(){
   document.querySelector('.asosiy').classList.toggle('hide-active')
